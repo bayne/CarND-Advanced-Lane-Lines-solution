b04079ea7936f6fe30c10fd2457ec26d8a9cbbaf
Brian Payne <bpayne@uci.edu>
2017-04-29 22:56:02 -0700

    Added lane pixel detection via windows Added annotation of lane

--- main.py
+++ main.py
@@ -73,8 +73,12 @@ class Pipeline:
             gradient_y_threshold,
             gradient_magnitude_threshold,
             gradient_direction_threshold,
-            source_points
+            source_points,
+            window_margin,
+            window_min
     ) -> None:
+        self.__window_min = window_min
+        self.__window_margin = window_margin
         self.__source_points = source_points
         self.__ksize = ksize
         self.__gradient_x_threshold = gradient_x_threshold
@@ -222,6 +226,110 @@ class Pipeline:
         self.__image_saver.save('perspective', self.current_filename, image)
         return image, reverse_transformation_matrix
 
+    def __lane_pixels(self, image):
+
+        binary_warped = image
+
+        # Assuming you have created a warped binary image called "binary_warped"
+        # Take a histogram of the bottom half of the image
+        half = int(binary_warped.shape[0] / 2)
+        histogram = np.sum(binary_warped[half:, :], axis=0)
+        # Create an output image to draw on and  visualize the result
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0] / 2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set height of windows
+        window_height = np.int(binary_warped.shape[0] / nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Set the width of the windows +/- margin
+        margin = self.__window_margin
+        # Set minimum number of pixels found to recenter window
+        minpix = self.__window_min
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+            win_y_high = binary_warped.shape[0] - window * window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (
+            nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (
+            nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
+        left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+        self.__image_saver.save(filename=self.current_filename, sub_directory='lane_pixels', image=out_img)
+
+        return out_img, left_fitx, right_fitx, ploty
+
+    def __annotate_lane(self, source_image, warped_image, reverse_matrix, left_fitx, right_fitx, ploty):
+        # Create an image to draw the lines on
+        warp_zero = np.zeros_like(warped_image).astype(np.uint8)
+        color_warp = warp_zero
+
+        # Recast the x and y points into usable format for cv2.fillPoly()
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+        # Warp the blank back to original image space using inverse perspective matrix (Minv)
+        newwarp = cv2.warpPerspective(color_warp, reverse_matrix, (source_image.shape[1], source_image.shape[0]))
+        # Combine the result with the original image
+        return cv2.addWeighted(source_image, 1, newwarp, 0.3, 0)
+
     def process(self, image):
         source_image = image.copy()
 
@@ -229,8 +337,12 @@ class Pipeline:
         image = self.__color_threshold(image)
         image = self.__edge_detection(image)
         image, reverse = self.__perspective_transform(image)
+        image, left_fitx, right_fitx, ploty = self.__lane_pixels(image)
+        image = self.__annotate_lane(source_image=source_image,warped_image=image, reverse_matrix=reverse,
+                                     left_fitx=left_fitx, right_fitx=right_fitx, ploty=ploty)
 
         return image
+
 
 
 def main():
@@ -278,7 +390,9 @@ def main():
             [703, 440],
             [1120, 700],
             [250, 700]
-        ])
+        ]),
+        window_margin=100,
+        window_min=50
     )
     test_images = glob.glob('./test_images/*.jpg')
 
